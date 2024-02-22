e5d936b6a6531c026058ee0501367795df0e9e1b
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2024-02-22 07:02:48 -0300

    Translating the names os the fields to english

--- invoice.py
+++ invoice.py
@@ -1,249 +1,301 @@
 class Invoice:
     @property
-    def numeroConvenio(self):
-        return self._numeroConvenio
+    def agreement_number(self):
+        """ numeroConvenio """
+        return self._agreement_number
 
     @property
-    def numeroCarteira(self):
-        return self._numeroCarteira
+    def wallet_number(self):
+        """ numeroCarteira """
+        return self._wallet_number
 
     @property
-    def numeroVariacaoCarteira(self):
-        return self._numeroVariacaoCarteira
+    def wallet_variation_number(self):
+        """ numeroVariacaoCarteira """
+        return self._wallet_variation_number
 
     @property
-    def codigoModalidade(self):
-        return self._codigoModalidade
+    def modality_code(self):
+        """ codigoModalidade """
+        return self._modality_code
 
     @property
-    def dataEmissao(self):
-        return self._dataEmissao
+    def issue_date(self):
+        """ dataEmissao """
+        return self._issue_date
 
     @property
-    def dataVencimento(self):
-        return self._dataVencimento
+    def expiration_date(self):
+        return self._expiration_date
 
     @property
-    def valorOriginal(self):
-        return self._valorOriginal
+    def original_value(self):
+        """ valorOriginal """
+        return self._original_value
 
     @property
-    def valorAbatimento(self):
-        return self._valorAbatimento
+    def rebate_value(self):
+        return self._rebate_value
 
     @property
-    def quantidadeDiasProtesto(self):
-        return self._quantidadeDiasProtesto
+    def number_of_protest_days(self):
+        """ quantidadeDiasProtesto """
+        return self._number_of_protest_days
 
     @property
-    def quantidadeDiasNegativacao(self):
-        return self._quantidadeDiasNegativacao
+    def number_of_days_to_negative(self):
+        """ quantidadeDiasNegativacao """
+        return self._number_of_days_to_negative
 
     @property
-    def orgaoNegativador(self):
-        return self._orgaoNegativador
+    def negative_organ(self):
+        """ orgaoNegativador """
+        return self._negative_organ
 
     @property
-    def indicadorAceiteTituloVencido(self):
-        return self._indicadorAceiteTituloVencido
+    def indicator_accepts_expired_title(self):
+        """ indicadorAceiteTituloVencido """
+        return self._indicator_accepts_expired_title
 
     @property
-    def numeroDiasLimiteRecebimento(self):
-        return self._numeroDiasLimiteRecebimento
+    def number_of_days_receiving_deadline(self):
+        """ numeroDiasLimiteRecebimento """
+        return self._number_of_days_receiving_deadline
 
     @property
-    def codigoAceite(self):
-        return self._codigoAceite
+    def accept_code(self):
+        """ codigoAceite """
+        return self._accept_code
 
     @property
-    def codigoTipoTitulo(self):
-        return self._codigoTipoTitulo
+    def title_type_code(self):
+        """ codigoTipoTitulo """
+        return self._title_type_code
 
     @property
-    def descricaoTipoTitulo(self):
-        return self._descricaoTipoTitulo
+    def description_type_title(self):
+        """ descricaoTipoTitulo """
+        return self._description_type_title
 
     @property
-    def indicadorPermissaoRecebimentoParcial(self):
-        return self._indicadorPermissaoRecebimentoParcial
+    def partial_receipt_permission_indicator(self):
+        """ indicadorPermissaoRecebimentoParcial """
+        return self._partial_receipt_permission_indicator
 
     @property
-    def numeroTituloBeneficiario(self):
-        return self._numeroTituloBeneficiario
+    def beneficiary_title_number(self):
+        """ numeroTituloBeneficiario """
+        return self._beneficiary_title_number
 
     @property
-    def numeroTituloCliente(self):
-        return self._numeroTituloCliente
+    def customer_title_number(self):
+        """ numeroTituloCliente """
+        return self._customer_title_number
 
     @property
-    def desconto(self):
-        return self._desconto
+    def discount(self):
+        """ desconto """
+        return self._discount
 
     @property
-    def segundoDesconto(self):
-        return self._segundoDesconto
+    def second_discount(self):
+        """ segundoDesconto """
+        return self._second_discount
 
     @property
-    def terceiroDesconto(self):
-        return self._terceiroDesconto
+    def third_discount(self):
+        """ terceiroDesconto """
+        return self._third_discount
 
     @property
-    def jurosMora(self):
-        return self._jurosMora
+    def late_payment_interest(self):
+        """ jurosMora """
+        return self._late_payment_interest
 
     @property
-    def multa(self):
-        return self._multa
+    def fine(self):
+        """ multa """
+        return self._fine
 
     @property
-    def pagador(self):
-        return self._pagador
+    def payer(self):
+        """ pagador """
+        return self._payer
 
     @property
-    def beneficiarioFinal(self):
-        return self._beneficiarioFinal
+    def final_beneficiary(self):
+        """ beneficiarioFinal """
+        return self._final_beneficiary
 
     @property
-    def indicadorPix(self):
-        return self._indicadorPix
+    def pix_indicator(self):
+        """ indicadorPix """
+        return self._pix_indicator
 
-    @numeroConvenio.setter
-    def numeroConvenio(self, value):
-        self._numeroConvenio = value
+    @agreement_number.setter
+    def agreement_number(self, value):
+        """ numeroConvenio """
+        self._agreement_number = value
 
-    @numeroCarteira.setter
-    def numeroCarteira(self, value):
-        self._numeroCarteira = value
+    @wallet_number.setter
+    def wallet_number(self, value):
+        """ numeroCarteira """
+        self._wallet_number = value
 
-    @numeroVariacaoCarteira.setter
-    def numeroVariacaoCarteira(self, value):
-        self._numeroVariacaoCarteira = value
+    @wallet_variation_number.setter
+    def wallet_variation_number(self, value):
+        """ numeroVariacaoCarteira """
+        self._wallet_variation_number = value
 
-    @codigoModalidade.setter
-    def codigoModalidade(self, value):
-        self._codigoModalidade = value
+    @modality_code.setter
+    def modality_code(self, value):
+        """ codigoModalidade """
+        self._modality_code = value
 
-    @dataEmissao.setter
-    def dataEmissao(self, value):
-        self._dataEmissao = value
+    @issue_date.setter
+    def issue_date(self, value):
+        """ dataEmissao """
+        self._issue_date = value
 
-    @dataVencimento.setter
-    def dataVencimento(self, value):
-        self._dataVencimento = value
+    @expiration_date.setter
+    def expiration_date(self, value):
+        """ dataVencimento """
+        self._expiration_date = value
 
-    @valorOriginal.setter
-    def valorOriginal(self, value):
-        self._valorOriginal = value
+    @original_value.setter
+    def original_value(self, value):
+        """ valorOriginal """
+        self._original_value = value
 
-    @valorAbatimento.setter
-    def valorAbatimento(self, value):
-        self._valorAbatimento = value
+    @rebate_value.setter
+    def rebate_value(self, value):
+        """ valorAbatimento """
+        self._rebate_value = value
 
-    @quantidadeDiasProtesto.setter
-    def quantidadeDiasProtesto(self, value):
-        self._quantidadeDiasProtesto = value
+    @number_of_protest_days.setter
+    def number_of_protest_days(self, value):
+        """ quantidadeDiasProtesto """
+        self._number_of_protest_days = value
 
-    @quantidadeDiasNegativacao.setter
-    def quantidadeDiasNegativacao(self, value):
-        self._quantidadeDiasNegativacao = value
+    @number_of_days_to_negative.setter
+    def number_of_days_to_negative(self, value):
+        """ quantidadeDiasNegativacao """
+        self._number_of_days_to_negative = value
 
-    @orgaoNegativador.setter
-    def orgaoNegativador(self, value):
-        self._orgaoNegativador = value
+    @negative_organ.setter
+    def negative_organ(self, value):
+        """ orgaoNegativador """
+        self._negative_organ = value
 
-    @indicadorAceiteTituloVencido.setter
-    def indicadorAceiteTituloVencido(self, value):
-        self._indicadorAceiteTituloVencido = value
+    @indicator_accepts_expired_title.setter
+    def indicator_accepts_expired_title(self, value):
+        """ indicadorAceiteTituloVencido """
+        self._indicator_accepts_expired_title = value
 
-    @numeroDiasLimiteRecebimento.setter
-    def numeroDiasLimiteRecebimento(self, value):
-        self._numeroDiasLimiteRecebimento = value
+    @number_of_days_receiving_deadline.setter
+    def number_of_days_receiving_deadline(self, value):
+        """ numeroDiasLimiteRecebimento """
+        self._number_of_days_receiving_deadline = value
 
-    @codigoAceite.setter
-    def codigoAceite(self, value):
-        self._codigoAceite = value
+    @accept_code.setter
+    def accept_code(self, value):
+        """ codigoAceite """
+        self._accept_code = value
 
-    @codigoTipoTitulo.setter
-    def codigoTipoTitulo(self, value):
-        self._codigoTipoTitulo = value
+    @title_type_code.setter
+    def title_type_code(self, value):
+        """ codigoTipoTitulo """
+        self._title_type_code = value
 
-    @descricaoTipoTitulo.setter
-    def descricaoTipoTitulo(self, value):
-        self._descricaoTipoTitulo = value
+    @description_type_title.setter
+    def description_type_title(self, value):
+        """ descricaoTipoTitulo """
+        self._description_type_title = value
 
-    @indicadorPermissaoRecebimentoParcial.setter
-    def indicadorPermissaoRecebimentoParcial(self, value):
-        self._indicadorPermissaoRecebimentoParcial = value
+    @partial_receipt_permission_indicator.setter
+    def partial_receipt_permission_indicator(self, value):
+        """ indicadorPermissaoRecebimentoParcial """
+        self._partial_receipt_permission_indicator = value
 
-    @numeroTituloBeneficiario.setter
-    def numeroTituloBeneficiario(self, value):
-        self._numeroTituloBeneficiario = value
+    @beneficiary_title_number.setter
+    def beneficiary_title_number(self, value):
+        """ numeroTituloBeneficiario """
+        self._beneficiary_title_number = value
 
-    @numeroTituloCliente.setter
-    def numeroTituloCliente(self, value):
-        self._numeroTituloCliente = value
+    @customer_title_number.setter
+    def customer_title_number(self, value):
+        """ numeroTituloCliente """
+        self._customer_title_number = value
 
-    @desconto.setter
-    def desconto(self, value):
-        self._desconto = value
+    @discount.setter
+    def discount(self, value):
+        """ desconto """
+        self._discount = value
 
-    @segundoDesconto.setter
-    def segundoDesconto(self, value):
-        self._segundoDesconto = value
+    @second_discount.setter
+    def second_discount(self, value):
+        """ segundoDesconto """
+        self._second_discount = value
 
-    @terceiroDesconto.setter
-    def terceiroDesconto(self, value):
-        self._terceiroDesconto = value
+    @third_discount.setter
+    def third_discount(self, value):
+        """ terceiroDesconto """
+        self._third_discount = value
 
-    @jurosMora.setter
-    def jurosMora(self, value):
-        self._jurosMora = value
+    @late_payment_interest.setter
+    def late_payment_interest(self, value):
+        """ jurosMora """
+        self._late_payment_interest = value
 
-    @multa.setter
-    def multa(self, value):
-        self._multa = value
+    @fine.setter
+    def fine(self, value):
+        """ multa """
+        self._fine = value
 
-    @pagador.setter
-    def pagador(self, value):
-        self._pagador = value
+    @payer.setter
+    def payer(self, value):
+        """ pagador """
+        self._payer = value
 
-    @beneficiarioFinal.setter
-    def beneficiarioFinal(self, value):
-        self._beneficiarioFinal = value
+    @final_beneficiary.setter
+    def final_beneficiary(self, value):
+        """ beneficiarioFinal """
+        self._final_beneficiary = value
 
-    @indicadorPix.setter
-    def indicadorPix(self, value):
-        self._indicadorPix = value
+    @pix_indicator.setter
+    def pix_indicator(self, value):
+        """ indicadorPix """
+        self._pix_indicator = value
 
     def to_dict(self):
         result = {
-            "numeroConvenio": self.numeroConvenio,
-            "numeroCarteira": self.numeroCarteira,
-            "numeroVariacaoCarteira": self.numeroVariacaoCarteira,
-            "codigoModalidade": self.codigoModalidade,
-            "dataEmissao": self.dataEmissao,
-            "dataVencimento": self.dataVencimento,
-            "valorOriginal": self.valorOriginal,
-            "valorAbatimento": self.valorAbatimento,
-            "quantidadeDiasProtesto": self.quantidadeDiasProtesto,
-            "quantidadeDiasNegativacao": self.quantidadeDiasNegativacao,
-            "orgaoNegativador": self.orgaoNegativador,
-            "indicadorAceiteTituloVencido": self.indicadorAceiteTituloVencido,
-            "numeroDiasLimiteRecebimento": self.numeroDiasLimiteRecebimento,
-            "codigoAceite": self.codigoAceite,
-            "codigoTipoTitulo": self.codigoTipoTitulo,
-            "descricaoTipoTitulo": self.descricaoTipoTitulo,
-            "indicadorPermissaoRecebimentoParcial": self.indicadorPermissaoRecebimentoParcial,
-            "numeroTituloBeneficiario": self.numeroTituloBeneficiario,
-            "numeroTituloCliente": self.numeroTituloCliente,
-            "desconto": self.desconto,
-            "segundoDesconto": self.segundoDesconto,
-            "terceiroDesconto": self.terceiroDesconto,
-            "jurosMora": self.jurosMora,
-            "multa": self.multa,
-            "pagador": self.pagador,
-            "beneficiarioFinal": self.beneficiarioFinal,
-            "indicadorPix": self.indicadorPix
+            "numeroConvenio": self.agreement_number,
+            "numeroCarteira": self.wallet_number,
+            "numeroVariacaoCarteira": self.wallet_variation_number,
+            "codigoModalidade": self.modality_code,
+            "dataEmissao": self.issue_date,
+            "dataVencimento": self.expiration_date,
+            "valorOriginal": self.original_value,
+            "valorAbatimento": self.rebate_value,
+            "quantidadeDiasProtesto": self.number_of_protest_days,
+            "quantidadeDiasNegativacao": self.number_of_days_to_negative,
+            "orgaoNegativador": self.negative_organ,
+            "indicadorAceiteTituloVencido": self.indicator_accepts_expired_title,
+            "numeroDiasLimiteRecebimento": self.number_of_days_receiving_deadline,
+            "codigoAceite": self.accept_code,
+            "codigoTipoTitulo": self.title_type_code,
+            "descricaoTipoTitulo": self.description_type_title,
+            "indicadorPermissaoRecebimentoParcial": self.partial_receipt_permission_indicator,
+            "numeroTituloBeneficiario": self.beneficiary_title_number,
+            "numeroTituloCliente": self.customer_title_number,
+            "desconto": self.discount,
+            "segundoDesconto": self.second_discount,
+            "terceiroDesconto": self.third_discount,
+            "jurosMora": self.late_payment_interest,
+            "multa": self.fine,
+            "pagador": self.payer,
+            "beneficiarioFinal": self.final_beneficiary,
+            "indicadorPix": self.pix_indicator
         }
         return result
 
@@ -312,48 +364,48 @@ if __name__ == '__main__':
 
     invoice_instance = Invoice()
 
-    invoice_instance.numeroConvenio = 3128557
-    invoice_instance.numeroCarteira = 17
-    invoice_instance.numeroVariacaoCarteira = 35
-    invoice_instance.codigoModalidade = 1
-    invoice_instance.dataEmissao = "15.02.2024"
-    invoice_instance.dataVencimento = "31.03.2024"
-    invoice_instance.valorOriginal = 123.45
-    invoice_instance.valorAbatimento = 12.34
-    invoice_instance.quantidadeDiasProtesto = 0
-    invoice_instance.quantidadeDiasNegativacao = 0
-    invoice_instance.orgaoNegativador = 10
-    invoice_instance.indicadorAceiteTituloVencido = "S"
-    invoice_instance.numeroDiasLimiteRecebimento = 1
-    invoice_instance.codigoAceite = "N"
-    invoice_instance.codigoTipoTitulo = 2
-    invoice_instance.descricaoTipoTitulo = "DM"
-    invoice_instance.indicadorPermissaoRecebimentoParcial = "S"
-    invoice_instance.numeroTituloBeneficiario = "2A584SDGTE8JN2G"
-    invoice_instance.numeroTituloCliente = "00031285570689531552"
-    invoice_instance.desconto = {
+    invoice_instance.agreement_number = 3128557
+    invoice_instance.wallet_number = 17
+    invoice_instance.wallet_variation_number = 35
+    invoice_instance.modality_code = 1
+    invoice_instance.issue_date = "15.02.2024"
+    invoice_instance.expiration_date = "31.03.2024"
+    invoice_instance.original_value = 123.45
+    invoice_instance.rebate_value = 12.34
+    invoice_instance.number_of_protest_days = 0
+    invoice_instance.number_of_days_to_negative = 0
+    invoice_instance.negative_organ = 10
+    invoice_instance.indicator_accepts_expired_title = "S"
+    invoice_instance.number_of_days_receiving_deadline = 1
+    invoice_instance.accept_code = "N"
+    invoice_instance.title_type_code = 2
+    invoice_instance.description_type_title = "DM"
+    invoice_instance.partial_receipt_permission_indicator = "S"
+    invoice_instance.beneficiary_title_number = "2A584SDGTE8JN2G"
+    invoice_instance.customer_title_number = "00031285570689531552"
+    invoice_instance.discount = {
         "tipo": 2,
         "dataExpiracao": "28.02.2024",
         "porcentagem": 5
     }
-    invoice_instance.segundoDesconto = {
+    invoice_instance.second_discount = {
         "dataExpiracao": "10.03.2024",
         "porcentagem": 4
     }
-    invoice_instance.terceiroDesconto = {
+    invoice_instance.third_discount = {
         "dataExpiracao": "20.03.2024",
         "porcentagem": 3
     }
-    invoice_instance.jurosMora = {
+    invoice_instance.late_payment_interest = {
         "tipo": 2,
         "porcentagem": 1.00
     }
-    invoice_instance.multa = {
+    invoice_instance.fine = {
         "tipo": 1,
         "data": "01.04.2024",
         "valor": 10.00
     }
-    invoice_instance.pagador = {
+    invoice_instance.payer = {
         "tipoInscricao": 1,
         "numeroInscricao": 97965940132,
         "nome": "Odorico Paraguassu",
@@ -364,11 +416,11 @@ if __name__ == '__main__':
         "uf": "TO",
         "telefone": "63987654321"
     }
-    invoice_instance.beneficiarioFinal = {
+    invoice_instance.final_beneficiary = {
         "tipoInscricao": 2,
         "numeroInscricao": 74910037000193,
         "nome": "Dirceu Borboleta"
     }
-    invoice_instance.indicadorPix = "N"
+    invoice_instance.pix_indicator = "N"
 
     print(invoice_instance.to_dict() == invoice)
