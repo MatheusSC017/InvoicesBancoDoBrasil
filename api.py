a8d973eb8ab1509e60cb04550c25bc155d8b08b6
Matheus Simão Caixeta <matheussimao2101@gmail.com>
2024-04-18 20:29:44 -0300

    Adding method to update the invoice

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import json
 import requests
 import os
 import dotenv
+from datetime import timedelta, date
 from utils import singleton
 
 dotenv.load_dotenv()
@@ -96,6 +97,19 @@ class BBChargingAPI:
         response = requests.post(self.base_url + '/boletos', data=json.dumps(invoice_info), headers=headers)
         return response.content
 
+    def update_invoice(self, billing_title_number, invoice_info):
+        token = self._get_token(scope_info=False)
+        application_key = os.getenv('APPLICATION_KEY')
+
+        headers = {
+            'Authorization': token,
+            'X-Developer-Application-Key': application_key,
+            'Accept': 'application/json',
+        }
+        response = requests.patch(f'{self.base_url}/boletos/{billing_title_number}', data=json.dumps(invoice_info), headers=headers)
+
+        return response.content
+
 
 if __name__ == '__main__':
     invoice = {
@@ -103,24 +117,24 @@ if __name__ == '__main__':
         "numeroCarteira": 17,
         "numeroVariacaoCarteira": 35,
         "codigoModalidade": 1,
-        "dataEmissao": "15.02.2024",
-        "dataVencimento": "31.03.2024",
+        "dataEmissao": '.'.join(reversed(str(date.today()).split('-'))),
+        "dataVencimento": '.'.join(reversed(str(date.today() + timedelta(days=60)).split('-'))),
         "valorOriginal": 123.45,
         "valorAbatimento": 12.34,
-        "quantidadeDiasProtesto": 5,
+        "quantidadeDiasProtesto": 0,
         "quantidadeDiasNegativacao": 5,
         "orgaoNegativador": 10,
         "indicadorAceiteTituloVencido": "S",
-        "numeroDiasLimiteRecebimento": 1,
+        "numeroDiasLimiteRecebimento": 30,
         "codigoAceite": "N",
         "codigoTipoTitulo": 2,
         "descricaoTipoTitulo": "DM",
         "indicadorPermissaoRecebimentoParcial": "S",
         "numeroTituloBeneficiario": "2A584SDGTE8JN2G",
-        "numeroTituloCliente": "00031285570689531598",
+        "numeroTituloCliente": "00031285570689531501",
         "desconto": {
             "tipo": 2,
-            "dataExpiracao": "28.02.2024",
+            "dataExpiracao": '.'.join(reversed(str(date.today() + timedelta(days=10)).split('-'))),
             "porcentagem": 5.0
         },
         "jurosMora": {
@@ -129,7 +143,7 @@ if __name__ == '__main__':
         },
         "multa": {
             "tipo": 1,
-            "data": "01.04.2024",
+            "data": '.'.join(reversed(str(date.today() + timedelta(days=65)).split('-'))),
             "valor": 10.00
         },
         "pagador": {
@@ -150,16 +164,104 @@ if __name__ == '__main__':
         },
         "indicadorPix": "N",
         "segundoDesconto": {
-            "dataExpiracao": "10.03.2024",
+            "dataExpiracao": '.'.join(reversed(str(date.today() + timedelta(days=30)).split('-'))),
             "porcentagem": 4.0
         },
         "terceiroDesconto": {
-            "dataExpiracao": "20.03.2024",
+            "dataExpiracao": '.'.join(reversed(str(date.today() + timedelta(days=50)).split('-'))),
             "porcentagem": 3.0
         },
     }
 
+    invoice_2 = {
+        "numeroConvenio": 3128557,
+        "indicadorNovaDataVencimento": "N",
+        "alteracaoData": {
+        "novaDataVencimento": '.'.join(reversed(str(date.today() + timedelta(days=55)).split('-')))
+        },
+        "indicadorAtribuirDesconto": "S",
+        "desconto": {
+        "tipoPrimeiroDesconto": 1,
+        "valorPrimeiroDesconto": 5,
+        "dataPrimeiroDesconto": '.'.join(reversed(str(date.today() + timedelta(days=10)).split('-'))),
+        "tipoSegundoDesconto": 1,
+        "valorSegundoDesconto": 4,
+        "dataSegundoDesconto": '.'.join(reversed(str(date.today() + timedelta(days=30)).split('-'))),
+        "tipoTerceiroDesconto": 1,
+        "valorTerceiroDesconto": 3,
+        "dataTerceiroDesconto": '.'.join(reversed(str(date.today() + timedelta(days=50)).split('-')))
+        },
+        "indicadorAlterarDesconto": "S",
+        "alteracaoDesconto": {
+        "tipoPrimeiroDesconto": 1,
+        "novoValorPrimeiroDesconto": 20,
+        "novaDataLimitePrimeiroDesconto": '.'.join(reversed(str(date.today() + timedelta(days=10)).split('-'))),
+        "tipoSegundoDesconto": 1,
+        "novoValorSegundoDesconto": 10,
+        "novaDataLimiteSegundoDesconto": '.'.join(reversed(str(date.today() + timedelta(days=30)).split('-'))),
+        "tipoTerceiroDesconto": 1,
+        "novoValorTerceiroDesconto": 5,
+        "novaDataLimiteTerceiroDesconto": '.'.join(reversed(str(date.today() + timedelta(days=45)).split('-'))),
+        },
+        "indicadorAlterarDataDesconto": "S",
+        "alteracaoDataDesconto": {
+        "novaDataLimitePrimeiroDesconto": '.'.join(reversed(str(date.today() + timedelta(days=20)).split('-'))),
+        "novaDataLimiteSegundoDesconto": '.'.join(reversed(str(date.today() + timedelta(days=40)).split('-'))),
+        "novaDataLimiteTerceiroDesconto": '.'.join(reversed(str(date.today() + timedelta(days=60)).split('-'))),
+        },
+        "indicadorProtestar": "N",
+        "protesto": {
+            "quantidadeDiasProtesto": 0
+        },
+        "indicadorSustacaoProtesto": "N",
+        "indicadorCancelarProtesto": "S",
+        "indicadorIncluirAbatimento": "S",
+        "abatimento": {
+            "valorAbatimento": 12.34
+        },
+        "indicadorAlterarAbatimento": "S",
+        "alteracaoAbatimento": {
+            "novoValorAbatimento": 10
+        },
+        "indicadorCobrarJuros": "S",
+        "juros": {
+            "tipoJuros": 2,
+            "taxaJuros": 10
+        },
+        "indicadorDispensarJuros": "N",
+        "indicadorCobrarMulta": "S",
+        "multa": {
+            "tipoMulta": 1,
+            "valorMulta": 10.00,
+            "dataInicioMulta": '.'.join(reversed(str(date.today() + timedelta(days=60)).split('-'))),
+            "taxaMulta": 10.00
+        },
+        "indicadorDispensarMulta": "N",
+        "indicadorNegativar": "S",
+        "negativacao": {
+            "quantidadeDiasNegativacao": 15,
+            "tipoNegativacao": 1
+        },
+        "indicadorAlterarSeuNumero": "S",
+        "alteracaoSeuNumero": {
+            "codigoSeuNumero": "BA695S1GTE8JN2A"
+        },
+        "indicadorAlterarEnderecoPagador": "S",
+        "alteracaoEndereco": {
+            "enderecoPagador": "Avenida São Cristovão",
+            "bairroPagador": "Centro",
+            "cidadePagador": "Sucupira",
+            "UFPagador": "TO",
+            "CEPPagador": 77458000
+        },
+        "indicadorAlterarPrazoBoletoVencido": "S",
+        "alteracaoPrazo": {
+            "quantidadeDiasAceite": 30
+        }
+    }
+
     bb_charging_api = BBChargingAPI(test_environment=True)
-    print(bb_charging_api.get_invoices('A', 452, 123873))
-    print(bb_charging_api.register_invoice(invoice))
-    print(bb_charging_api.get_invoice('3128557', '00031285570689531598'))
+    # print(bb_charging_api.get_invoices('A', 452, 123873))
+    # print(bb_charging_api.register_invoice(invoice))
+    print(bb_charging_api.get_invoice('3128557', '00031285570689531501'))
+    print(bb_charging_api.update_invoice('00031285570689531501', invoice_2))
